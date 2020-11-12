bdf5087ce242e45accc5afe545fd4b4a4d2cef4c
Alex <ag3rlitz@gmail.com>
2020-11-12 17:25:03 -0500

    built out mychart.js, added local config of chart.min.js, e.preventdefault to force chart to persist, QueryValue of form in title

--- js/myChart.js
+++ js/myChart.js
@@ -1,10 +1,14 @@
 var ctx = $('#myChart');
-console.log(ctx);
+var defaults = { "positive": 30, "neutral": 40, "negative": 50, "query": "Sample"};
 
-var jsonObject = { "positive": 30, "neutral": 40, "negative": 50, "query": "election"}
-loadChart(jsonObject)
+$('#queryForm').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    jsonObject = { "positive": 50, "neutral": 10, "negative": 20, "query": formData.get("queryValue")};
+    loadChart(jsonObject);
+});
 
-function loadChart(jsonObject){
+function loadChart(defaults){
     var chart = new Chart(ctx, {
         // The type of chart we want to create
         type: 'pie',
@@ -16,7 +20,7 @@ function loadChart(jsonObject){
                 'Positive'
             ],
             datasets: [{
-                data: [jsonObject.negative, jsonObject.neutral, jsonObject.positive],
+                data: [defaults.negative, defaults.neutral, defaults.positive],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.3)',
                     'rgba(255, 206, 86, 0.3)',
@@ -33,7 +37,7 @@ function loadChart(jsonObject){
         options: {
             title: {
                 display: true,
-                text: (jsonObject.query + " chart").toUpperCase()
+                text: (defaults.query + " chart").toUpperCase()
             }
         }
     });
